7290aa70bf1158885217904dced2dd19ec97371f
kine6ct <aurokine6c@gmail.com>
2023-10-04 19:25:24 +0000

    premiere tache

---
+++ lecode.py
@@ -0,0 +1,5 @@
+#creer un generateur de mot de passe
+# 1- on demande a l'utilisateur le nombre de mot de passe qu'il souhaite reçevoir
+
+nbr_de_mdp= int(input("Donnez le nombre de mot de passe que vous souhaiter recevoir: "))
+print(nbr_de_mdp, "mot de passe")
