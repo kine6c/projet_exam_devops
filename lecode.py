8ccef6c46f6a7de1038a9992ea2b98232f55bb14
kine6ct <aurokine6c@gmail.com>
2023-10-04 20:30:24 +0000

    deuxieme tache

--- lecode.py
+++ lecode.py
@@ -1,5 +1,18 @@
 #creer un generateur de mot de passe
-# 1- on demande a l'utilisateur le nombre de mot de passe qu'il souhaite reçevoir
+import random
 
-nbr_de_mdp= int(input("Donnez le nombre de mot de passe que vous souhaiter recevoir: "))
+print("------- Welcome to  Your Password Generator -------- ")
+# 1- on demande a l'utilisateur le nombre de mot de passe qu'il souhaite generer
+
+pattern= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^*&(),.?0123456789'
+
+nbr_de_mdp= int(input("Saisissez le nombre de mot de passe que vous souhaitez generer : "))
 print(nbr_de_mdp, "mot de passe")
+
+length= int(input("Veuillez saisir la longueur du mot de passe :"))
+
+for mot in range(nbr_de_mdp) :
+    motdepasse=''
+    for i in range(length)  :
+        motdepasse +=random.choice(pattern)   
+    print(f"VOICI VOS MOTS DE PASSE :{motdepasse}")
